af737ff8c313a20c662766bf15a02fcfb166b27e
mehdiarejdal <mehdi.arejdal@gmail.com>
2024-05-04 20:12:20 +0100

    commit

--- backend_dews/urls.py
+++ backend_dews/urls.py
@@ -17,14 +17,35 @@ Including another URLconf
 # backend_dews/urls.py
 from django.urls import path
 from .views.student_list_view import student_list_view
-from .views.showPredictionsBaseline1 import predictions_view, single_student_prediction_view
-
+from .views.students_list import student_list
+from .views.prediction_M_1_2 import (
+    predictions_M_1_2Baseline,
+    single_student_prediction_M_1_2Baseline,
+    predictions_M_1_2Undrsampling,
+    single_student_prediction_M_1_2Undrsampling,
+)
+from .views.prediction_M_1_1 import (
+    predictions_M_1_1Baseline,
+    single_student_prediction_M_1_1Baseline,
+    predictions_M_1_1Undrsampling,
+    single_student_prediction_M_1_1Undrsampling,
+)
 
 urlpatterns = [
     path('students/', student_list_view, name='student_list'),
-    path('predictions/', predictions_view, name='predictions'),
-    path('predictions/<str:student_id>/', single_student_prediction_view, name='single_student_prediction'),
+    path('studentslist/', student_list, name='student_list'),
+    # level 7 
+    # predictions after 1 year
+    path('predictions/M_1_1Baseline/', predictions_M_1_1Baseline, name='predictions_M_1_2Baseline'),
+    path('predictions/M_1_1Baseline/<str:student_id>/', single_student_prediction_M_1_1Baseline, name='single_student_prediction_M_1_2Baseline'),
+    path('predictions/M_1_1Undrsampling/', predictions_M_1_1Undrsampling, name='predictions_M_1_2Undrsampling'),
+    path('predictions/M_1_1Undrsampling/<str:student_id>/', single_student_prediction_M_1_1Undrsampling, name='single_student_prediction_M_1_2Undrsampling'),
+    # predictions after 2 years
+    path('predictions/M_1_2Baseline/', predictions_M_1_2Baseline, name='predictions_M_1_2Baseline'),
+    path('predictions/M_1_2Baseline/<str:student_id>/', single_student_prediction_M_1_2Baseline, name='single_student_prediction_M_1_2Baseline'),
+    path('predictions/M_1_2Undrsampling/', predictions_M_1_2Undrsampling, name='predictions_M_1_2Undrsampling'),
+    path('predictions/M_1_2Undrsampling/<str:student_id>/', single_student_prediction_M_1_2Undrsampling, name='single_student_prediction_M_1_2Undrsampling'),
 
-    # Add more URL patterns as needed
+
 ]
 
--- backend_dews/views/prediction_M_1_2.py
+++ backend_dews/views/prediction_M_1_2.py
@@ -8,13 +8,12 @@ from elasticsearch_utils.utils import get_es_client
 from Feature_engineering import Feature_engineering
 
 @csrf_exempt
-def predictions_view(request):
+def make_predictions(request, model_path, index_name):
     # Load the ML model
-    loaded_model = mlflow.sklearn.load_model("./Baseline")
+    loaded_model = mlflow.sklearn.load_model(model_path)
 
     # Retrieve data from Elasticsearch index
     es_client = get_es_client()
-    index_name = "data_middle_1"
     es_query = {"query": {"match_all": {}}}
     es_response = es_client.search(index=index_name, body=es_query)
 
@@ -27,32 +26,32 @@ def predictions_view(request):
 
     # Make predictions
     predictions = loaded_model.predict(df)
+    probabilities = loaded_model.predict_proba(df)
+    predicted_probabilities = probabilities[:, 1]
 
     # Convert predictions to response format
-    response_data = [{'student_id': hit['_source']['id_eleve'], 'prediction': int(predictions[i])} for i, hit in enumerate(es_data)]
-    
-    # Map predictions to "Success" or "Not Success"
-    for data in response_data:
-        data['prediction'] = "Success" if data['prediction'] == 0 else "Not Success"
+    response_data = [{'student_id': hit['_source']['id_eleve'],
+                      'prediction': "Success" if prediction == 1 else "Not Success",
+                      'probability': round(probability, 2)} 
+                     for prediction, probability, hit in zip(predictions, predicted_probabilities, es_data)]
 
     # Construct HTML response
     html_response = "<h1>Predictions</h1>"
-    html_response += "<table border='1'><tr><th>Student ID</th><th>Prediction</th></tr>"
+    html_response += "<table border='1'><tr><th>Student ID</th><th>Prediction</th><th>Probability</th></tr>"
     for data in response_data:
-        html_response += f"<tr><td>{data['student_id']}</td><td>{data['prediction']}</td></tr>"
+        html_response += f"<tr><td>{data['student_id']}</td><td>{data['prediction']}</td><td>{data['probability']}</td></tr>"
     html_response += "</table>"
 
     # Return HTML response
     return HttpResponse(html_response)
 
 @csrf_exempt
-def single_student_prediction_view(request, student_id):
+def single_student_prediction(request, model_path, index_name, student_id):
     # Load the ML model
-    loaded_model = mlflow.sklearn.load_model("./Baseline")
+    loaded_model = mlflow.sklearn.load_model(model_path)
 
     # Retrieve data from Elasticsearch index for the specified student
     es_client = get_es_client()
-    index_name = "data_middle_1"
     es_query = {"query": {"match": {"id_eleve": student_id}}}
     es_response = es_client.search(index=index_name, body=es_query)
 
@@ -75,3 +74,9 @@ def single_student_prediction_view(request, student_id):
 
     # Return HTML response
     return HttpResponse(html_response)
+
+# Define views for different models and endpoints
+predictions_M_1_2Baseline = lambda request: make_predictions(request, "./Models/level_7/M_1_2/Baseline", "data_middle_1")
+single_student_prediction_M_1_2Baseline = lambda request, student_id: single_student_prediction(request, "./Models/level_7/M_1_2/Baseline", "data_middle_1", student_id)
+predictions_M_1_2Undrsampling = lambda request: make_predictions(request, "./Models/level_7/M_1_2/Undersampling", "data_middle_1")
+single_student_prediction_M_1_2Undrsampling = lambda request, student_id: single_student_prediction(request, "./Models/level_7/M_1_2/Undersampling", "data_middle_1", student_id)
